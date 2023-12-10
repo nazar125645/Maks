bd369225a5ff7bfdd63a8785e6c54c6bc5df9596
nazar125645 <98477696+nazar125645@users.noreply.github.com>
2023-12-10 16:47:49 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (false - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "27.06.2005"; // Дата рождения
+var fullname = "Пасічник Максим Сергійович" // ФИО
+var name = "Максим" // Имя
+
+// Паспорт
+var passport_id = "006418776" // Номер паспорта
+
+// КПП
+var kpp_id = "3931903562" // ИНН
+
+// Права
+var rights_valid_until = "11.9.2025" // Права "Дійсні до"
+var rights_categories = "A, A1, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "27.12.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
